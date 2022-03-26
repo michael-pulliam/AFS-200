9149f7ca74557f22ebc06b10b2313fae39efa23d
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2022-03-26 11:30:56 -0500

    updated apples.py

--- week1/apples.py
+++ week1/apples.py
@@ -5,7 +5,6 @@ apple_price = '{:.2f}'.format(applePrice)
 user_name = input('What is your name? ')
 # Store the response from the user in a variable as a string.
 print('Hello, ' + user_name)
-
 # Tell the user the cost of an apple.
 # Ask the user how many apples they would like to purchase and store this value in a variable as an integer.
 buy_num_apples = int(input(f'Apples cost ${apple_price} ea. How many would you like to purchase? '))
@@ -15,11 +14,12 @@ total_after_tax = total_price * 1.08
 print( f"Your total today after tax is: ${total_after_tax:.2f}")
 # how much they give to pay with
 payment = float(input("How much are you paying with? "))
-print("Out of $" + str(payment))
+total_payment = "{:.2f}".format(payment)
+print("Out of $" + str(total_payment))
 # How much change customer gets back
 changeReturn = payment - total_after_tax
 change_return = "{:.2f}".format(changeReturn)
-print(f"A total of $" + str(change_return) +" is your change." )
+print(f"A total of $" + str(change_return) +" is your change.") 
 # Thank the user for their purchase.  The message should contain the users name,
 print(f'Thank you for your purchase of ' + str(buy_num_apples) + ' apples at a cost of $' + str(apple_price) + ' each. ' 'Have a Great Day ' + user_name + '!')
 
