87dd219d1adb385c0312b4067fc74bddfeac827d
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2022-04-18 17:32:14 -0500

    updated yahtzee.py for mastery

--- week4/assignments/yahtzee.py
+++ week4/assignments/yahtzee.py
@@ -15,7 +15,7 @@ class Die:
         self.current_face_value = roll
     
     def displayFaceValue(self):
-        print(f"({self.current_face_value})")
+        print(f"({self.current_face_value})", end=" ")
         
     def roll(self):
         self.setCurrentFaceValue(random.randint(1, self.__sides_count))
@@ -23,8 +23,16 @@ class Die:
     def __str__(self):
         return str('\nA rolling die with {0} sides'.format(self.__sides_count))
 
-fourSided = Die(6,0)
-
+fourSided = Die(1,0)
+active = True
+roll_result= []
 print(fourSided)
 for x in range(5):
-    print(f'{fourSided.roll()}', end=" ")
+    fourSided.roll()
+    fourSided.displayFaceValue()
+    roll_result.append(fourSided.getCurrentFaceValue())
+for roll in roll_result:
+    if roll_result[0] != roll:
+        active = False
+if active == True:
+    print(f"\nYahtzee!!!")
